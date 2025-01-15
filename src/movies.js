128562a5303d170c62d0f6dc2f2ed8535ee5e3dd
Simo <simeonov.pa@gmail.com>
2025-01-15 22:52:38 +0000

    add dynamic details link for each movie

--- src/movies.js
+++ src/movies.js
@@ -1,17 +1,20 @@
 export default [
     {
+        id: 1,
         title: 'Jungle Cuise',
         genre: 'Adventure',
         description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
         imageUrl: '/static/img/jungle-cruise.jpeg',
     },
-    {
+    {   
+        id: 2,
         title: 'Sonic the Hedgehog 3',
         genre: 'Fantasy',
         description: 'Sonic, Knuckles, and Tails reunite against a powerful new adversary, Shadow, a mysterious villain with powers unlike anything they have faced before. With their abilities outmatched, Team Sonic must seek out an unlikely alliance..',
         imageUrl: '/static/img/sonic-the-hedgehog-three.jpg',
     },
     {
+        id: 3,
         title: 'Gladiator II',
         genre: 'Action',
         description: 'After his home is conquered by the tyrannical emperors who now lead Rome, Lucius is forced to enter the Colosseum and must look to his past to find strength to return the glory of Rome to its people.',
