49713ef78acb06b957406ace22bf1101db11169e
Simo <simeonov.pa@gmail.com>
2025-01-16 00:17:58 +0000

    parce form data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -8,14 +8,21 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+    //console.log(newMovie);
+    
+    res.end();
+});
+
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
 
     // Get movie data for movieId
     const movie = movieService.findOne(movieId);
-    console.log(movie);
+    //console.log(movie);
 
-    res.render('details');
+    res.render('details', { movie });
 });
 
 export default movieController;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use('/static', express.static('src/public')); // set path: 'static' for css request with path static/css/style.css to search instead in public
+app.use(express.urlencoded({ extended: false })); // Learn express to parse form data
 
 app.use(routes);
 
