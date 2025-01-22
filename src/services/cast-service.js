5a6d23a4d59dd71a87dae5b9e28ce95e0d717ee3
Simo <simeonov.pa@gmail.com>
2025-01-22 22:07:00 +0000

    exclude casts where all ready attached

--- src/services/cast-service.js
+++ src/services/cast-service.js
@@ -1,8 +1,15 @@
 import Cast from "../models/Cast.js";
 
 export default {
-    getAll() {
-        return Cast.find({});
+    getAll(filter = {}) {
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            // query = query.find({_id: {$nin: filter.exclude}});   // one way / MongoDB way
+            query = query.nin('_id', filter.exclude);   // second way / Mongoose way
+        }
+        return query;
+        // return Cast.find({});
     },
     create(castData) {
         // TODO: Create cast
